0439f5c4746a8279e4f1424ba712157519abab1e
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-12-06 19:27:54 +0100

    Fixat upplägget på första sidan.

--- Hemsida/assets/pages/home.php
+++ Hemsida/assets/pages/home.php
@@ -1,3 +1,10 @@
 <div class="paper">			
-	<p>Första sidan.</p>
+	<h2>Hem</h2>
+	<hr>
+	<h3 class="menu">Vill du veta vad din lön blir?</h3>
+	<p>Bacon ipsum dolor sit amet sausage brisket boudin, short ribs prosciutto shankle corned beef tenderloin flank bresaola jerky. Turkey pastrami biltong ham doner, turducken pancetta pork belly ribeye kielbasa brisket. Beef corned beef bresaola, ribeye turducken kielbasa filet mignon tenderloin brisket flank strip steak bacon. Tri-tip turducken fatback, capicola spare ribs corned beef leberkas pork loin. Tri-tip pork loin corned beef sirloin salami brisket frankfurter chuck leberkas hamburger meatball.</p>
+	<h3 class="menu">Vill du vet hur allt går till?</h3>
+	<p>Bacon ipsum dolor sit amet sausage brisket boudin, short ribs prosciutto shankle corned beef tenderloin flank bresaola jerky. Turkey pastrami biltong ham doner, turducken pancetta pork belly ribeye kielbasa brisket. Beef corned beef bresaola, ribeye turducken kielbasa filet mignon tenderloin brisket flank strip steak bacon. Tri-tip turducken fatback, capicola spare ribs corned beef leberkas pork loin. Tri-tip pork loin corned beef sirloin salami brisket frankfurter chuck leberkas hamburger meatball.</p>
+	<h3 class="menu">Är du ett företag som letar anställda?</h3>
+	<p>Bacon ipsum dolor sit amet sausage brisket boudin, short ribs prosciutto shankle corned beef tenderloin flank bresaola jerky. Turkey pastrami biltong ham doner, turducken pancetta pork belly ribeye kielbasa brisket. Beef corned beef bresaola, ribeye turducken kielbasa filet mignon tenderloin brisket flank strip steak bacon. Tri-tip turducken fatback, capicola spare ribs corned beef leberkas pork loin. Tri-tip pork loin corned beef sirloin salami brisket frankfurter chuck leberkas hamburger meatball.</p>
 </div>
