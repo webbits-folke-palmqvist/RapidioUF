9795eea73a6bf1594ff303ef9c94243011f28920
Joen Berg <joen.berg@webbits.nu>
2012-12-06 18:55:57 +0100

    Ändrade sidan 404

--- Hemsida/assets/pages/404.php
+++ Hemsida/assets/pages/404.php
@@ -1,6 +1,7 @@
 <div class="paper">
 	<center>
 		<h1>404</h1>
-		<h3>Sidan du försökte gå in på hittades inte.</h3>
+		<h3>ATTANS!</h3>
+		<h3>Sidan eller filen du försökte nå hittades inte.</h3>
 	</center>
 </div>
