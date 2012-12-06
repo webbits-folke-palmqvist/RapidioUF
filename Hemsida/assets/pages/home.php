5b4e2900230b5e733921b90ba129d3005f2da4f9
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-12-06 19:58:35 +0100

    Fixade boxarna på första sidan

--- Hemsida/assets/pages/home.php
+++ Hemsida/assets/pages/home.php
@@ -2,13 +2,16 @@
 	<div class="box3">
 		<h3 class="menu">Vad blir din lön?</h3>
 		<p>Bacon ipsum dolor sit amet sausage brisket boudin, short ribs prosciutto shankle corned beef tenderloin flank bresaola jerky. Turkey pastrami biltong ham doner, turducken pancetta pork belly ribeye kielbasa brisket. Beef corned beef bresaola, ribeye turducken kielbasa filet mignon tenderloin brisket flank strip steak bacon. Tri-tip turducken fatback, capicola spare ribs corned beef leberkas pork loin. Tri-tip pork loin corned beef sirloin salami brisket frankfurter chuck leberkas hamburger meatball.</p>
+		<a href="#">Länk</a>
 	</div>
 	<div class="box3">
 		<h3 class="menu">Är du ett företag?</h3>
 		<p>Bacon ipsum dolor sit amet sausage brisket boudin, short ribs prosciutto shankle corned beef tenderloin flank bresaola jerky. Turkey pastrami biltong ham doner, turducken pancetta pork belly ribeye kielbasa brisket. Beef corned beef bresaola, ribeye turducken kielbasa filet mignon tenderloin brisket flank strip steak bacon. Tri-tip turducken fatback, capicola spare ribs corned beef leberkas pork loin. Tri-tip pork loin corned beef sirloin salami brisket frankfurter chuck leberkas hamburger meatball.</p>
+		<a href="#">Länk</a>
 	</div>
 	<div class="box3">
 		<h3 class="menu">Hur går till?</h3>
 		<p>Bacon ipsum dolor sit amet sausage brisket boudin, short ribs prosciutto shankle corned beef tenderloin flank bresaola jerky. Turkey pastrami biltong ham doner, turducken pancetta pork belly ribeye kielbasa brisket. Beef corned beef bresaola, ribeye turducken kielbasa filet mignon tenderloin brisket flank strip steak bacon. Tri-tip turducken fatback, capicola spare ribs corned beef leberkas pork loin. Tri-tip pork loin corned beef sirloin salami brisket frankfurter chuck leberkas hamburger meatball.</p>
+		<a href="#">Länk</a>
 	</div>
 </div>
