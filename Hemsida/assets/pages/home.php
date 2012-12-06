604425ac428e41c168fb7d5e92a966b040e7ffca
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-12-06 19:45:18 +0100

    Ändrade texten på första sidan

--- Hemsida/assets/pages/home.php
+++ Hemsida/assets/pages/home.php
@@ -1,14 +1,14 @@
 <div class="box">
 	<div class="box3">
-		<h3 class="menu">Vill du veta vad din lön blir?</h3>
+		<h3 class="menu">Vad blir din lön?</h3>
 		<p>Bacon ipsum dolor sit amet sausage brisket boudin, short ribs prosciutto shankle corned beef tenderloin flank bresaola jerky. Turkey pastrami biltong ham doner, turducken pancetta pork belly ribeye kielbasa brisket. Beef corned beef bresaola, ribeye turducken kielbasa filet mignon tenderloin brisket flank strip steak bacon. Tri-tip turducken fatback, capicola spare ribs corned beef leberkas pork loin. Tri-tip pork loin corned beef sirloin salami brisket frankfurter chuck leberkas hamburger meatball.</p>
 	</div>
 	<div class="box3">
-		<h3 class="menu">Är du ett företag som letar anställda?</h3>
+		<h3 class="menu">Är du ett företag?</h3>
 		<p>Bacon ipsum dolor sit amet sausage brisket boudin, short ribs prosciutto shankle corned beef tenderloin flank bresaola jerky. Turkey pastrami biltong ham doner, turducken pancetta pork belly ribeye kielbasa brisket. Beef corned beef bresaola, ribeye turducken kielbasa filet mignon tenderloin brisket flank strip steak bacon. Tri-tip turducken fatback, capicola spare ribs corned beef leberkas pork loin. Tri-tip pork loin corned beef sirloin salami brisket frankfurter chuck leberkas hamburger meatball.</p>
 	</div>
 	<div class="box3">
-		<h3 class="menu">Vill du vet hur allt går till?</h3>
+		<h3 class="menu">Hur går till?</h3>
 		<p>Bacon ipsum dolor sit amet sausage brisket boudin, short ribs prosciutto shankle corned beef tenderloin flank bresaola jerky. Turkey pastrami biltong ham doner, turducken pancetta pork belly ribeye kielbasa brisket. Beef corned beef bresaola, ribeye turducken kielbasa filet mignon tenderloin brisket flank strip steak bacon. Tri-tip turducken fatback, capicola spare ribs corned beef leberkas pork loin. Tri-tip pork loin corned beef sirloin salami brisket frankfurter chuck leberkas hamburger meatball.</p>
 	</div>
 </div>
