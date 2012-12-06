a43680330bc1334ae23c2061f77ac14d2dc612ea
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-12-06 19:14:32 +0100

    Utseende och upplägget på kontakt
    Fixade både utseendet och upplägget på kontakt sidan.

--- Hemsida/assets/pages/contact.php
+++ Hemsida/assets/pages/contact.php
@@ -1,3 +1,17 @@
 <div class="paper">
-	<p>Kontakt</p>
+	<h3>Kontakt</h3>
+	<hr class="menu">
+	<p>Här finner du all info om hur man kontaktar oss.</p>
+	<p>
+		<strong>För rekrytering</strong> <i>- Letar du efter jobb?</i><br>
+		Victoria.Wiebols@rapidio.se 
+	</p>
+	<p>
+		<strong>För företag</strong> <i>- Vill du vara med och få arbetare?</i><br>
+		Leila.Zlailouh@rapidio.se
+	</p>
+	<p>
+		<strong>För hjälp</strong> <i>- Här hjälper vi dig med allt!</i><br>
+		info@rapidio.se
+	</p>
 </div>
