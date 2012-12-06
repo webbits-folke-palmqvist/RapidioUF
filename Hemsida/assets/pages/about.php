45e45aa5dfd958b23655f2825d9a30984bf63498
Joen Berg <joen.berg@webbits.nu>
2012-12-06 19:30:21 +0100

    Fixad "Om oss" sidan

--- Hemsida/assets/pages/about.php
+++ Hemsida/assets/pages/about.php
@@ -1,3 +1,27 @@
 <div class="paper">
 	<p>Om oss</p>
+	<hr>
+	<img src="http://existenzmc.se//img/users/avatar/new.png">
+	<strong>Förnamn</strong> efternamn
+	<p>Bacon ipsum dolor sit amet short ribs brisket hamburger 
+		pancetta pig pastrami ham hock short loin drumstick sausage 
+		jerky turkey. Chuck tail tongue, filet mignon short ribs chicken 
+		shankle tenderloin cow rump hamburger doner turkey brisket.
+		 Pork chop pork loin bacon jerky pork ham shankle andouille biltong.
+		  Swine pancetta leberkas frankfurter beef short ribs prosciutto capicola
+		   hamburger ball tip. Meatloaf kielbasa ground round hamburger chicken
+		    frankfurter bresaola biltong. Pork belly corned beef andouille ham hock meatball ground round doner.</p>
+<hr>
+	<img src="http://existenzmc.se//img/users/avatar/new.png">
+		<strong>Förnamn</strong> efternamn
+		<p>Bacon ipsum dolor sit amet short ribs brisket hamburger 
+		pancetta pig pastrami ham hock short loin drumstick sausage 
+		jerky turkey. Chuck tail tongue, filet mignon short ribs chicken 
+		shankle tenderloin cow rump hamburger doner turkey brisket.
+		 Pork chop pork loin bacon jerky pork ham shankle andouille biltong.
+		  Swine pancetta leberkas frankfurter beef short ribs prosciutto capicola
+		   hamburger ball tip. Meatloaf kielbasa ground round hamburger chicken
+		    frankfurter bresaola biltong. Pork belly corned beef andouille ham hock meatball ground round doner.</p>
+	<hr>
+
 </div>
