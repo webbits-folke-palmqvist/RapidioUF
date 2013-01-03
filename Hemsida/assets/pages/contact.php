e04cb254a692be1d31e98f50e810fd068d88c1fb
Folke Palmqvist <folke.palmqvist@webbits.nu>
2013-01-03 20:34:29 +0100

    Fixade mail länk

--- Hemsida/assets/pages/contact.php
+++ Hemsida/assets/pages/contact.php
@@ -1,13 +1,13 @@
 <p>Här finner du all info om hur man kontaktar oss.</p>
 <p>
 	<strong>För rekrytering</strong> <i>- Letar du efter jobb?</i><br>
-	Victoria.Wiebols@rapidio.se 
+	<a href="mailto:Victoria.Wiebols@rapidio.se">Victoria.Wiebols@rapidio.se</a> 
 </p>
 <p>
 	<strong>För företag</strong> <i>- Vill du vara med och få arbetare?</i><br>
-	Leila.Zlailouh@rapidio.se
+	<a href="mailto:Leila.Zlailouh@rapidio.se">Leila.Zlailouh@rapidio.se</a>
 </p>
 <p>
 	<strong>För hjälp</strong> <i>- Här hjälper vi dig med allt!</i><br>
-	info@rapidio.se
+	<a href="mailto:info@rapidio.se">info@rapidio.se</a>
 </p>
